15add1597a61a7ab4cbfa453aa7637cbcfca7ad5
ayase <paper.airplane01@gmail.com>
2019-01-07 06:15:42 +0900

    Introduce TypeScript.

--- ts/chat.ts
+++ ts/chat.ts
@@ -1,43 +1,47 @@
-"use strict";
-window.onload = function () {
-    if (!window["WebSocket"]) {
-        alert("エラー: WebSocketに対応していないブラウザです。");
+window.onload = function() {
+    if (!(window as any)["WebSocket"]) {
+        alert("エラー: WebSocketに対応していないブラウザです。" );
     }
     else {
-        let socket = new WebSocket("ws://" + location.host + "/room"); // WebSocket接続を開始する。
-        socket.onclose = function () {
+        let socket = new WebSocket("ws://" + location.host + "/room");  // WebSocket接続を開始する。
+        socket.onclose = function() {
             alert("接続が終了しました。");
-        };
-        socket.onmessage = function (e) {
+        }
+        socket.onmessage = function(e) {
             const msg = JSON.parse(e.data);
             makeChatDOM(msg);
-        };
+        }
         listenSubmit(socket);
     }
-};
-function makeChatDOM(msg) {
+}
+
+function makeChatDOM(msg: Readonly<any>): void {
     let chatImg = document.createElement("img");
     chatImg.setAttribute("title", msg.Name);
     chatImg.style.width = "50px";
     chatImg.style.verticalAlign = "middle";
     chatImg.setAttribute("src", msg.AvatarURL);
+    
     let spanMsg = document.createElement("span");
     spanMsg.textContent = msg.Message;
+
     let chatMsg = document.createElement("li");
     chatMsg.appendChild(chatImg);
     chatMsg.appendChild(spanMsg);
+
     let messages = document.getElementById("messages");
     if (messages != null) {
-        messages.appendChild(chatMsg);
+        messages.appendChild(chatMsg)
     }
     else {
         alert("id=messagesが見つかりませんでした。");
     }
 }
-function listenSubmit(socket) {
-    let callback = function (event) {
-        let msgbox = document.querySelector("#chatbox textarea");
-        socket.send(JSON.stringify({ "Message": msgbox.value }));
+
+function listenSubmit(socket: WebSocket): void {
+    let callback = function(event: Event) {
+        let msgbox = <HTMLInputElement>document.querySelector("#chatbox textarea");
+        socket.send(JSON.stringify({"Message": msgbox.value}));
         msgbox.value = "";
         event.preventDefault();
     };
