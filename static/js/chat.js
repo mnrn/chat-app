0f4ef98dff8700b5682e5eb32c8d5ab13437769b
ayase <paper.airplane01@gmail.com>
2019-01-06 15:00:36 +0900

    Separate functions.

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,8 +1,42 @@
-$(function(){
+window.onload = function(){
     let socket   = null;
-    let msgbox   = $("#chatbox textarea");
-    let messages = $("#messages");
+    if (!window["WebSocket"]) {
+        alert("エラー: WebSocketに対応していないブラウザです。" );
+    }
+    else {
+        socket = new WebSocket("ws://" + location.host + "/room");  // WebSocket接続を開始する。
+        socket.onclose = function() {
+            alert("接続が終了しました。");
+        }
+        socket.onmessage = function(e) {
+            const msg = JSON.parse(e.data);
+            makeChatDOM(msg);
+        }
+    }
+    submit(socket);
+}
+
+function makeChatDOM(msg) {
+    let chatImg = document.createElement("img");
+    chatImg.setAttribute("title", msg.Name);
+    chatImg.style.width = "50px";
+    chatImg.style.verticalAlign = "middle";
+    chatImg.setAttribute("src", msg.AvatarURL);
+    
+    let spanMsg = document.createElement("span");
+    spanMsg.textContent = msg.Message;
+
+    let chatMsg = document.createElement("li");
+    chatMsg.appendChild(chatImg);
+    chatMsg.appendChild(spanMsg);
+
+    let messages = document.getElementById("messages");
+    messages.appendChild(chatMsg);
+}
+
+function submit(socket) {
     $("#chatbox").submit(function() {
+        let msgbox  = $("#chatbox textarea");
         if (!msgbox.val()) {
             alert("メッセージを入力してください。");
             return false;
@@ -15,26 +49,4 @@ $(function(){
         msgbox.val("");
         return false;
     });
-    if (!window["WebSocket"]) {
-        alert("エラー: WebSocketに対応していないブラウザです。" );
-    }
-    else {
-        socket = new WebSocket("ws://" + location.host + "/room");  // WebSocket接続を開始する。
-        // WebSocketがメッセージを受信するとjQueryを使って箇条書きの要素にメッセージが追加され、ブラウザ上表示されます。
-        socket.onclose = function() {
-            alert("接続が終了しました。");
-        }
-        socket.onmessage = function(e) {
-            const msg = JSON.parse(e.data);
-            messages.append(
-                $("<li>").append(
-                    $("<img>").attr("title", msg.Name).css({
-                        width: 50,
-                        verticalAlign: "middile"
-                    }).attr("src", msg.AvatarURL),
-                    $("<span>").text(msg.Message)
-                )
-            );
-        }
-    }
-});
+}
